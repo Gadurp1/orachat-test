003aeffcd108527f7e09e46ae4fa3dca4ca84970
Gadurp1 <drexelunderwood@Drexels-MacBook-Pro.local>
2016-10-20 21:34:22 -0500

    Initial Authentication
    basic login and user controller

---
+++ app/Api/Controllers/Authcontroller.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Api\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuthExceptions\JWTException;
+use Auth;
+use Validator;
+use App\User;
+
+class AuthController extends \App\Http\Controllers\Controller
+{
+
+    public function index()
+    {
+        // TODO: show users
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+        $user=Auth::user();
+        $user->token=$token;
+        return response()->json(['success'=>true,'data'=>$user]);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('users/login', '\App\Api\Controllers\AuthController@authenticate');
+Route::post('users/register', '\App\Api\Controllers\AuthController@register');
+
+Route::group(['middleware' => 'jwt.auth'], function()
+{
+    Route::get('users/me', '\App\Http\Controllers\UserController@index');
+    Route::post('users/me', '\App\Http\Controllers\UserController@update');
+});
